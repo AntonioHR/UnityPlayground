be0c5d8c9bfce1eb0db62a7e015cb8591b45d27c
Antonio Harger Rivero <antonio.harger@gmail.com>
2019-02-19 23:47:50 -0300

    Added basic repository with the simple animation player utility and example

---
+++ Assets/AntonioHR/AnimationHelpers/AnimationQueuePlayable.cs
@@ -0,0 +1,49 @@
+﻿using AntonioHR.AnimationHelpers.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace AntonioHR.AnimationHelpers
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimationQueuePlayable : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationClip[] clips;
+
+
+        private PlayableGraph playableGraph;
+        private PlayQueuePlayable playQueue;
+
+        private void Start()
+        {
+            playableGraph = PlayableGraph.Create();
+
+            var playQueuePlayable = ScriptPlayable<PlayQueuePlayable>.Create(playableGraph);
+            playQueue = playQueuePlayable.GetBehaviour();
+            playQueue.Initialize(clips, playQueuePlayable, playableGraph, OnNewClip);
+
+            var playableOutput = AnimationPlayableOutput.Create(playableGraph, "Animation", GetComponent<Animator>());
+
+            playableOutput.SetSourcePlayable(playQueuePlayable);
+            playableOutput.SetSourceOutputPort(0);
+
+            playableGraph.Play();
+        }
+
+        private void OnDestroy()
+        {
+            playableGraph.Destroy();
+        }
+
+        private void OnNewClip()
+        {
+            Debug.Log("New Clip!");
+        }
+    }
+}
